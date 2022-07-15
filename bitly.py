3845826d7438b94cecaf88caee82f9836bb6c395
nkhramtsov <nkramtsov@gmail.com>
2022-07-15 11:41:11 +0500

    Add new version of script

--- bitly.py
+++ bitly.py
@@ -1,12 +1,14 @@
+from dotenv import load_dotenv
+import argparse
 import os
 import requests
 from urllib.parse import urlparse
+import sys
 
 API_BITLY_URL = 'https://api-ssl.bitly.com/v4/bitlinks/'
 
 
 def shorten_link(token, url):
-
     headers = {'Authorization': f'Bearer {token}'}
     payload = {'long_url': url}
 
@@ -19,15 +21,16 @@ def shorten_link(token, url):
 
 
 def count_clicks(token, link):
-
     parsed_link = urlparse(link)
     headers = {'Authorization': f'Bearer {token}'}
     payload = {'unit': 'day', 'units': -1}
 
     response = requests.get(
         f'{API_BITLY_URL}'
-        f'{parsed_link.netloc}{parsed_link.path}/clicks/summary',
-        headers=headers, params=payload)
+        f'{parsed_link.netloc}/{parsed_link.path}/clicks/summary',
+        headers=headers,
+        params=payload
+    )
     response.raise_for_status()
 
     total_clicks = response.json()['total_clicks']
@@ -36,26 +39,33 @@ def count_clicks(token, link):
 
 
 def is_bitlink(url):
-
     parsed_url = urlparse(url)
 
     response = requests.get(
-        f'{API_BITLY_URL}{parsed_url.netloc}{parsed_url.path}')
+        f'{API_BITLY_URL}{parsed_url.netloc}/{parsed_url.path}'
+    )
 
     return response.ok
 
-if __name__ == "__main__":
 
-    token = os.environ['BITLY_TOKEN']
-    print('Введите ссылку')
-    inputed_url = input()
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('link', type=str)
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    token = os.getenv('BITLY_TOKEN')
+    parser = parse_args()
 
     try:
-        if is_bitlink(inputed_url):
-            total_clicks = count_clicks(token, inputed_url)
+        if is_bitlink(parser.link):
+            total_clicks = count_clicks(token, parser.link)
             print('Кликов', total_clicks)
         else:
-            bitlink = shorten_link(token, inputed_url)
+            bitlink = shorten_link(token, parser.link)
             print('Битлинк', bitlink)
     except requests.exceptions.HTTPError:
         print('Ошибка')
